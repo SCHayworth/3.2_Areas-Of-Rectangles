b57dfd79187b2b37d2b6c12ac169ba38340669c0
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-01 21:50:34 -0700

    Text changes
    Added line breaks. Changed the print statement on line 25 to read, "Both rectangles have the same area!"

--- area-of-rectangles.py
+++ area-of-rectangles.py
@@ -12,14 +12,14 @@ rect_a_width = float(input('What is the width of Rectangle A in feet? '))
 rect_a_area = rect_a_length * rect_a_width
 
 # Get length and width of rectangle B from user, and calculate its area.
-rect_b_length = float(input('What is the length of Rectangle B in feet? '))
+rect_b_length = float(input('\nWhat is the length of Rectangle B in feet? '))
 rect_b_width = float(input('What is the width of Rectangle B in feet? '))
 rect_b_area = rect_b_length * rect_b_width
 
 # Compare the areas of both rectangles and display which one has the greater area, or if they are equal.
 if rect_a_area > rect_b_area:
-  print(f'Rectangle A has the greatest area: {rect_a_area:.2f} sq. feet.')
+  print(f'\nRectangle A has the greatest area: {rect_a_area:.2f} sq. feet.')
 elif rect_a_area < rect_b_area: 
-  print(f'Rectangle B has the greatest area: {rect_b_area:.2f} sq. feet.')
+  print(f'\nRectangle B has the greatest area: {rect_b_area:.2f} sq. feet.')
 else:
-  print(f'Rectangle A has an area of {rect_a_area:.2f} sq. feet and Rectangle B has an area of {rect_b_area:.2f} sq. feet.')
+  print(f'\nBoth rectangles have the same area!')
